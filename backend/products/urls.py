60b2b03a9fcd07fa995d56d3fbe23de09e87e712
Олег Дидечкин <crm.29431@dz.com>
2023-05-07 15:49:25 +0300

    categories endpoint added

--- backend/products/urls.py
+++ backend/products/urls.py
@@ -1,8 +1,9 @@
-from .views import ProductsListView, ProductDetailsView
+from .views import CategoriesListView, ProductsListView, ProductDetailsView
 from django.urls import path, include
 
 
 urlpatterns = [
+    path("categories", CategoriesListView.as_view()),
     path("products", ProductsListView.as_view()),
     path("product/<slug:slug>", ProductDetailsView.as_view()),
 ]
